dde54d5ac852d6920c29248a63d7d2a576541be3
PetriskoJaroslav <petrisko.jaroslav.ml@gmail.com>
2024-05-30 21:57:43 +0200

    feat: Main Screen change

--- app/src/main/java/com/example/tryingfreerasp/MainActivity.kt
+++ app/src/main/java/com/example/tryingfreerasp/MainActivity.kt
@@ -3,11 +3,14 @@ package com.example.tryingfreerasp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.tryingfreerasp.ui.theme.TryingFreeRASPTheme
@@ -31,10 +34,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello World!",
-        modifier = modifier
-    )
+    Box(modifier.wrapContentSize(Alignment.Center)) {
+        Text(
+            text = "Hello kind person from Talsec!",
+            modifier = modifier
+        )
+    }
 }
 
 @Preview(showBackground = true)
