68860a8d93a883c873f65613168b097499a19ac6
PetriskoJaroslav <petrisko.jaroslav.ml@gmail.com>
2024-05-31 11:21:32 +0200

    feat: add warning screen

--- app/src/main/java/com/example/tryingfreerasp/TalsecApplication.kt
+++ app/src/main/java/com/example/tryingfreerasp/TalsecApplication.kt
@@ -2,13 +2,6 @@ package com.example.tryingfreerasp
 
 import android.app.Application
 import android.util.Log
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import com.aheaditec.talsec_security.security.api.Talsec
 import com.aheaditec.talsec_security.security.api.TalsecConfig
 import com.aheaditec.talsec_security.security.api.ThreatListener
@@ -16,11 +9,6 @@ import com.aheaditec.talsec_security.security.api.ThreatListener
 class TalsecApplication : Application(), ThreatListener.ThreatDetected {
     override fun onCreate() {
         super.onCreate()
-        print("lololka")
-
-        // Uncomment the following Log.e(...) to get your expectedSigningCertificateHashBase64
-        // Copy the result from logcat and assign to expectedSigningCertificateHashBase64
-        // Log.e("SigningCertificateHash", Utils.computeSigningCertificateHash(this))
 
         val config = TalsecConfig(
             expectedPackageName,
@@ -36,13 +24,17 @@ class TalsecApplication : Application(), ThreatListener.ThreatDetected {
 
     companion object {
         private const val expectedPackageName = "com.example.tryingfreerasp"
-        private val expectedSigningCertificateHashBase64 = arrayOf("dxfcgvhjbukilo") // None
+        private val expectedSigningCertificateHashBase64 = arrayOf("") // None
         private const val watcherMail = "petrisko.jaroslav.ml@gmail.com" // I guess it wont hurt to leave this here
         private val supportedAlternativeStores = emptyArray<String>()
         private val isProd = true
     }
+
+
     override fun onUntrustedInstallationSourceDetected() {
         Log.w("warning", "untrusted source detected")
+        SafeApplication.safeInstallation.value = false
+        SafeApplication.warningString = R.string.unauthorized_source
     }
 
     //Not supposed to do this but the class expects them to be implemented
